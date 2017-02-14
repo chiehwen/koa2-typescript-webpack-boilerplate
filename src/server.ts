aa7e5451237f31e66799eb9106e3a84600e46262
Chieh-Wen Yang <chuck.yang@hackathon.tw>
2017-02-14 14:15:05 +0800

    Add comment to server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,13 @@ import * as chalk from "chalk";
 
 import router from "./routes";
 
+// Create the app from the ES6 class
+// Koa application is now a class and requires the new operator.
 const app = new Koa();
 const port = process.env.PORT || 8080;
 
 app
+  // Body Parser Handler
   .use(bodyParser())
   .use(router.routes())
   .use(router.allowedMethods());
